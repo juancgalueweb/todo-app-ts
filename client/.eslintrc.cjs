88b14625337fe0203cb3921bb9f1bc06864f81f0
juancgalueweb <juancgalue@icloud.com>
2023-04-14 03:02:02 -0400

    Arreglando el pre-commit y el eslintrc.cjs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: '../tsconfig.json'
   },
   plugins: ['react'],
   rules: {
