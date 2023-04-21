cf7a620a1e1d5cb0cb9888dcd82e81ffd10d8bac
juancgalueweb <juancgalue@icloud.com>
2023-04-21 01:27:04 -0400

    La BD se conecta con MongoDB Atlas desde cualquier IP

--- server/src/config/dbConnection.ts
+++ server/src/config/dbConnection.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 mongoose.set('strictQuery', false)
 
-const LOCAL_DB = process.env.LOCAL_DB_URL
-// const DEPLOYMENT_DB = process.env.MONGO_URI
+// const LOCAL_DB = process.env.LOCAL_DB_URL
+const DEPLOYMENT_DB = process.env.MONGO_URI
 
 dbConnect().catch(err => {
   console.log(err)
@@ -10,8 +10,8 @@ dbConnect().catch(err => {
 
 export async function dbConnect(): Promise<void> {
   try {
-    if (LOCAL_DB !== undefined) {
-      await mongoose.connect(LOCAL_DB)
+    if (DEPLOYMENT_DB !== undefined) {
+      await mongoose.connect(DEPLOYMENT_DB)
     }
     console.log('Database connected 🔥🔥🔥')
   } catch (error) {
