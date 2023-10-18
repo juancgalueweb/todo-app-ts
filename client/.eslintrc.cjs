4eb21cd322101911d09cd1724f0a052353718df3
juancgalueweb <juancgalue@icloud.com>
2023-10-18 16:40:07 -0300

    Saca el client a una carpeta separada

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
   overrides: [],
   parserOptions: {
-    project: './tsconfig.json',
+    project: './client/tsconfig.json',
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    project: './server/tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
+  }
+}
