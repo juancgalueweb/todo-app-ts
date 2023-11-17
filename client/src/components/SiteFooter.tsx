6a03a12f6b439946bff7847006b95de962b5e609
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-11-17 14:32:18 -0400

    align correctiions on footer section

--- client/src/components/SiteFooter.tsx
+++ client/src/components/SiteFooter.tsx
@@ -9,50 +9,48 @@ function SiteFooter(): JSX.Element {
   const SOCIAL_ICON_SIZE = 30
   return (
     <SSiteFooter>
-      <div className='flex_container'>
-        <div className='footer_copyright'>
-          <p>
-            Copyright <CopyrightOutlined /> 2023{' '}
-            <a
-              href='https://www.linkedin.com/in/juancgalue/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Juan Carlos Galué
-            </a>
-          </p>
-        </div>
-        <div className='footer_social'>
-          <a
-            id='githubIcon'
-            href='https://github.com/juancgalueweb/todo-app-ts'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <GithubOutlined style={{ fontSize: SOCIAL_ICON_SIZE }} />
-          </a>
-          <a
-            id='linkedinIcon'
-            href='https://www.linkedin.com/in/juancgalue/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <LinkedinOutlined style={{ fontSize: SOCIAL_ICON_SIZE }} />
-          </a>
-        </div>
-        <div className='footer_mention'>
-          <p>
-            Responsive design by{' '}
-            <a
-              href='https://www.linkedin.com/in/marcopdonoso/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Marco Perez Donoso
-            </a>
-          </p>
-        </div>
+      {/* <div className='flex_container'> */}
+      <div className='footer_copyright'>
+        <p>
+          Copyright <CopyrightOutlined /> 2023{' '}
+        </p>
+        <a
+          href='https://www.linkedin.com/in/juancgalue/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Juan Carlos Galué
+        </a>
       </div>
+      <div className='footer_social'>
+        <a
+          id='githubIcon'
+          href='https://github.com/juancgalueweb/todo-app-ts'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <GithubOutlined style={{ fontSize: SOCIAL_ICON_SIZE }} />
+        </a>
+        <a
+          id='linkedinIcon'
+          href='https://www.linkedin.com/in/juancgalue/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <LinkedinOutlined style={{ fontSize: SOCIAL_ICON_SIZE }} />
+        </a>
+      </div>
+      <div className='footer_mention'>
+        <p>Responsive design by </p>
+        <a
+          href='https://www.linkedin.com/in/marcopdonoso/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Marco Perez Donoso
+        </a>
+      </div>
+      {/* </div> */}
     </SSiteFooter>
   )
 }
